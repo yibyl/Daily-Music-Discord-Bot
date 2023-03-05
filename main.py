0f5211b64d9d38d06ccf348f0b87c4e952669b6b
Unamedpickle <84691868+Unamedpickle@users.noreply.github.com>
2023-03-05 04:22:19 -0500

    Silence Command
    Pog

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import random
 from discord.ext import commands
 from discord.utils import get
 
-Token = ("MTA4MTgzOTU1MDU2Mjg0MDYyNg.Gmd3Pb.QyU42JstgsoXdzBoSthsbKe4GV4hW7w_OFerVw")
+Token = ("")
 intents = discord.Intents.default()
 intents.message_content = True
 bot = discord.Client(intents=intents)
@@ -16,6 +16,14 @@ bot = commands.Bot(command_prefix='$', intents=intents)
 async def on_ready():
     print(f'{bot.user} has connected to Discord!')
 
+@bot.event
+async def on_message(message):
+    if message.author.id == 238752815055372290:
+        await message.delete()
+        dm = await message.author.create_dm()
+        await message.channel.send("no speak " + message.author.name)
+
+    await bot.process_commands(message)
 
 bot.run(Token)
 
