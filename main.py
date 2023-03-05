cbebb099f2adeb05a0efac8b851e97cc5b9398e2
yibyl <bwei667@gmail.com>
2023-03-05 02:18:42 -0600

    added stuff

--- main.py
+++ main.py
@@ -1,4 +1,22 @@
+import os
+import discord
+import asyncio
+import random
 from dotenv import load_dotenv
+from discord.ext import commands
+from discord.utils import get
 
-if __name__ == '__main__':
-    pass
+load_dotenv()
+Token = ("MTA4MTgzOTU1MDU2Mjg0MDYyNg.Gep1HS.wsg09Q7yHOu00IfVvHxfkPy6zau8eqyIzq-MHc")
+intents = discord.Intents.default()
+intents.message_content = True
+bot = discord.Client(intents=intents)
+bot = commands.Bot(command_prefix='$', intents=intents)
+
+
+@bot.event
+async def on_ready():
+    print(f'{bot.user} has connected to Discord!')
+
+
+bot.run(Token)
