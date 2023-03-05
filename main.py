250ceca0637c64d46ec21d3d52848ea468c390f2
Unamedpickle <84691868+Unamedpickle@users.noreply.github.com>
2023-03-05 03:24:51 -0500

    Making the bot actually work
    pog

--- main.py
+++ main.py
@@ -1,14 +1,11 @@
-<<<<<<< Updated upstream
 import os
 import discord
 import asyncio
 import random
-from dotenv import load_dotenv
 from discord.ext import commands
 from discord.utils import get
 
-load_dotenv()
-Token = ("MTA4MTgzOTU1MDU2Mjg0MDYyNg.Gep1HS.wsg09Q7yHOu00IfVvHxfkPy6zau8eqyIzq-MHc")
+Token = ("MTA4MTgzOTU1MDU2Mjg0MDYyNg.Gmd3Pb.QyU42JstgsoXdzBoSthsbKe4GV4hW7w_OFerVw")
 intents = discord.Intents.default()
 intents.message_content = True
 bot = discord.Client(intents=intents)
@@ -21,22 +18,4 @@ async def on_ready():
 
 
 bot.run(Token)
-=======
-# This is a sample Python script.
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
-#cock and balls
->>>>>>> Stashed changes
